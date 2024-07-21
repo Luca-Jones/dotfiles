ab68ccb1f9351ccf6b8b3d21ff7758a739f45f17
Luca-Jones <132953140+Luca-Jones@users.noreply.github.com>
2024-07-21 12:05:00 -0700

    fixed file moving

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 pacman_packages=("gnome" "xorg" "i3" "neofetch" "curl" "zsh" "code" "fzf" "gedit" "vim" "polybar" "rofi" "bashtop" "cmatrix" "mpv")
 yay_packages=("feh" "picom" "cava" "spotify")
-config_files=("alacritty" "i3" "picom" "polybar" "rofi")
 
 # update packages
 sudo pacman -Syu --noconfirm
@@ -23,6 +22,8 @@ rm -rf yay
 # install yay packages
 yay -S ${yay_packages[@]} --noconfirm
 
+cd ~
+
 # git packages
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
@@ -32,13 +33,24 @@ git clone "https://github.com/pystardust/ani-cli.git"
 sudo cp ani-cli/ani-cli /usr/local/bin
 rm -rf ani-cli
 
+config_files=("alacritty" "i3" "picom" "polybar" "rofi")
+
+# move fonts
+sudo cp -r ~/dotfiles/0xProto /usr/share/fonts/
+
 # move config files
-cd ~/.config
-mkdir ./${config_files[@]}
 cd ~
-cp -r ~/dotfiles/${config_files[@]} ~/.config/
+for config in ${config_files[@]}
+do
+    echo $config
+    sudo cp -r ~/dotfiles/$config ~/.config
+done
+
 cp ~/dotfiles/zshrc ~/.zshrc
 cp ~/dotfiles/p10k.zsh ~/.p10k.zsh
+
+#oh-my-zsh extensions
+
 mkdir ~/Pictures/wallpapers
 cp ~/dotfiles/waterfront.jpg ~/Pictures/wallpapers/
 
