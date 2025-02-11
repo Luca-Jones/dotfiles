90abcdda293cd00ad202ed114986129ef7933a4c
luca <lucabuscagliajones@gmail.com>
2025-02-10 16:49:32 -0800

    update

---
+++ scripts/wal.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+wallpaper_path=$HOME/wallpapers
+wallpaper_name="$(ls $wallpaper_path | rofi -dmenu || pkill rofi)"
+
+if [[ -f $wallpaper_path/$wallpaper_name ]]; then
+    ln -sf "$wallpaper_path/$wallpaper_name" "$wallpaper_path/wallpaper"
+    wall-change "$wallpaper_path/wallpaper"
+else
+    exit 1
+fi3
